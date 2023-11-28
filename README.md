6a88deee6c21c8c3e31f14ed4742613f3c555d4d
Horea Christian <chr@chymera.eu>
2023-11-28 11:15:09 -0500

    [DATALAD] Recorded changes

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Gin: https://gin.g-node.org/TheChymera/mvmdna
 GitHub: https://github.com/TheChymera/mvmnda
+
+## Internal
+
+### Adding data
+
+Due to Git and Git-annex intricaciles, apparently “shared” mode causes issues, please put new data in `/mnt/DATA/data/studies/manish/incoming/mvmnda` on typhon.
--- rawdata/code/reposit.py
+++ rawdata/code/reposit.py
@@ -2,17 +2,21 @@ from datetime import datetime
 from dateutil import tz
 from pathlib import Path
 from neuroconv.converters import SpikeGLXConverterPipe
+import json
 
-folder_path = "/mnt/DATA/data/studies/manish/mvmnda/sourcedata/sub-pixelfiber/ses-01"
-#folder_path = "../../sourcedata/M387-2023-10-20_g0/M387-2023-10-20_g0_imec0/M387-2023-10-20_g0_tcat.imec0.ap.bin"
-#folder_path = "../../sourcedata/M387-2023-10-20_g0"
-converter = SpikeGLXConverterPipe(folder_path=folder_path)
+dir_path = "/mnt/DATA/data/studies/manish/mvmnda/sourcedata/Ach_DualPixel_7_14_23/Ach_DualPixel_7_14_23_g0"
+#dir_path = "/mnt/DATA/data/studies/manish/mvmnda/sourcedata/sub-pixelfiber/M387-2023-10-20_g0"
+#dir_path = "../../sourcedata/M387-2023-10-20_g0"
+converter = SpikeGLXConverterPipe(folder_path=dir_path)
 
 # Extract what metadata we can from the source files
 metadata = converter.get_metadata()
-# For data provenance we add the time zone information to the conversion
+
+# The following doesn't work as per the guide ( https://neuroconv.readthedocs.io/en/main/conversion_examples_gallery/recording/spikeglx.html#single-stream ), apparently no session_start_time is read in:
 #session_start_time = metadata["NWBFile"]["session_start_time"].replace(tzinfo=tz.gettz("US/Pacific"))
-#metadata["NWBFile"].update(session_start_time=session_start_time)
+# So we do:
+session_start_time = datetime.now(tz.tzlocal())
+metadata["NWBFile"].update(session_start_time=session_start_time)
 
 # Choose a path for saving the nwb file and run the conversion
 nwbfile_path = "/mnt/DATA/data/studies/manish/mvmnda/rawdata/my_spikeglx_session.nwb"
